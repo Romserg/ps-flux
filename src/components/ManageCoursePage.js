97ac8bcd8693d779bcfec534c4c172722128b710
Romserg <serg.romankov@gmail.com>
2019-08-08 19:32:56 +0300

    Add a form to create a custom course in a table

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Prompt } from "react-router-dom";
+import React, { useState } from "react";
+// import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 const MangeCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange({ target }) {
+    setCourse({ ...course, [target.name]: target.value });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      {/*<Prompt when={true} message="Are you sure you want to leave?" />*/}
+      <CourseForm course={course} onChange={handleChange}/>
     </>
   );
 };
