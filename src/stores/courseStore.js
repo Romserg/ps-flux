1a2b88b693f01e0d7efa73fd653fe03ccdb80707
Romserg <serg.romankov@gmail.com>
2019-08-14 22:51:26 +0300

    Create course store

---
+++ src/stores/courseStore.js
@@ -0,0 +1,42 @@
+import { EventEmitter } from 'events';
+import Dispatcher from '../appDispatcher';
+import actionTypes from "../actions/actionTypes";
+
+const CHANGE_EVENET = "change";
+let _courses = [];
+
+class CourseStore extends EventEmitter {
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENET, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENET, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENET);
+  }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCourseBySlug(slug) {
+    return _courses.find(course => course.slug === slug);
+  }
+}
+
+const store = new CourseStore();
+
+Dispatcher.register(action => {
+  switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    default:
+  }
+});
+
+export default store;
